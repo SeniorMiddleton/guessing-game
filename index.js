28364291980987063541b4efc730094eba718e73
SeniorMiddleton <kiselr647@gmail.com>
2023-09-21 23:48:07 +0300

    init: test

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const GuessingGame = require('./src/guessing-game.js');
+const GuessingGame = require("./src/guessing-game.js");
 
+game.setRange(0, 385);
+
+let guess;
+do {
+  guess = game.guess();
+  if (guess < 359) {
+    game.lower();
+  } else if (guess > 359) {
+    game.greater();
+  }
+} while (guess !== 359);
 window.game = new GuessingGame();
--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.guessValue = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.guessValue = Math.floor((this.min + this.max) / 2);
+    return this.guessValue;
+  }
 
-    guess() {
+  lower() {
+    // if (this.guessValue === this.max) {
 
-    }
+    //   this.max--;
+    // }
+    this.max = this.guessValue + 1;
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.guessValue;
+  }
 }
 
 module.exports = GuessingGame;
